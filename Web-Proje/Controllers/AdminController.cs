a777222d4e4b66d3738134ff13d4ebb5fb8c06eb
Muhammet Tarık ÖZTOPRAK <56087286+TarikOztoprak@users.noreply.github.com>
2020-12-30 20:44:47 +0300

    update

--- Web-Proje/Controllers/AdminController.cs
+++ Web-Proje/Controllers/AdminController.cs
@@ -4,15 +4,151 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web_Proje.Models;
 
 namespace Web_Proje.Controllers
 {
+    [Authorize(Roles="admin")]
     public class AdminController : Controller
     {
-        [Authorize(Roles = "admin")]
-        public IActionResult Index()
+        private readonly Context _context;
+
+        public AdminController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.books.ToListAsync());
+        }
+
+        // GET: Admin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var books = await _context.books
+                .FirstOrDefaultAsync(m => m.kitapID == id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            return View(books);
+        }
+
+        // GET: Admin/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("kitapID,kitapAdi,basimYili,yazar,sayfaSayisi,konu,link,kategori")] Books books)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(books);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(books);
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var books = await _context.books.FindAsync(id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+            return View(books);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("kitapID,kitapAdi,basimYili,yazar,sayfaSayisi,konu,link,kategori")] Books books)
+        {
+            if (id != books.kitapID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(books);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BooksExists(books.kitapID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(books);
+        }
+
+        // GET: Admin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var books = await _context.books
+                .FirstOrDefaultAsync(m => m.kitapID == id);
+            if (books == null)
+            {
+                return NotFound();
+            }
+
+            return View(books);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var books = await _context.books.FindAsync(id);
+            _context.books.Remove(books);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BooksExists(int id)
+        {
+            return _context.books.Any(e => e.kitapID == id);
         }
     }
 }
