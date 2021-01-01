a2989036837547d671096cbb8a71ee206fcef5dd
Muhammet Tarık ÖZTOPRAK <56087286+TarikOztoprak@users.noreply.github.com>
2021-01-01 17:39:23 +0300

    update

--- Web-Proje/Controllers/MessagesController.cs
+++ Web-Proje/Controllers/MessagesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -20,32 +21,18 @@ namespace Web_Proje.Controllers
         }
 
         // GET: Messages
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int id)
         {
+            ViewBag.id = id;
             return View(await _context.Messages.ToListAsync());
-        }
-
-        // GET: Messages/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var messages = await _context.Messages
-                .FirstOrDefaultAsync(m => m.messageID == id);
-            if (messages == null)
-            {
-                return NotFound();
-            }
-
-            return View(messages);
+                    
         }
 
         // GET: Messages/Create
-        public IActionResult Create()
+        [Authorize()]
+        public IActionResult Create(int id)
         {
+            ViewBag.id = id;
             return View();
         }
 
@@ -60,63 +47,15 @@ namespace Web_Proje.Controllers
             {
                 _context.Add(messages);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Create));
             }
             return View(messages);
         }
 
-        // GET: Messages/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var messages = await _context.Messages.FindAsync(id);
-            if (messages == null)
-            {
-                return NotFound();
-            }
-            return View(messages);
-        }
-
-        // POST: Messages/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("messageID,message,BookId,User")] Messages messages)
-        {
-            if (id != messages.messageID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(messages);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MessagesExists(messages.messageID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(messages);
-        }
 
         // GET: Messages/Delete/5
+        [Authorize(Roles="admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -137,12 +76,13 @@ namespace Web_Proje.Controllers
         // POST: Messages/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var messages = await _context.Messages.FindAsync(id);
             _context.Messages.Remove(messages);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Redirect("~/Home/Index");
         }
 
         private bool MessagesExists(int id)
